c8e1413a20288a642253bf5aa22a3b49851d4273
Mohamed Attar <mohammedattar0100020@gmail.com>
2022-11-30 21:29:08 +0200

    update api.php

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +12,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([ 'prefix' => 'admin', 'namespace' => '\App\Http\Controllers\Api\Admin'], function () {
+    // v1
+    Route::group(['prefix' => 'v1', 'namespace' => 'V1' , 'middleware' => ['auth:sanctum' , 'except' => ['auth']]], function () {
+    });
+    Route::post("/v1/auth" , '\App\Http\Controllers\Api\Admin\V1\authController@login');
 });
+Route::get("/test" , function(){return "Hi";});
