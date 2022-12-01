0c5f5ec0ce1c380e69b2be25014ac70c2626f0fb
Mohamed Attar <mohammedattar0100020@gmail.com>
2022-12-01 21:00:59 +0200

    set  token expiration time

--- api/app/Http/Controllers/Api/Admin/V1/authController.php
+++ api/app/Http/Controllers/Api/Admin/V1/authController.php
@@ -6,9 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\V1\authAdmin;
 use App\Http\Traits\HttpResponse;
 use App\Models\User;
+use DateInterval;
+use DateTime;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Request;
-
 class authController extends Controller
 {
     use HttpResponse;
@@ -19,9 +19,12 @@ class authController extends Controller
             return $this->error("Authentication failed" , 401);
         }
         $user = User::where("email" , $credentials['email'])->first(['id' , 'name' , 'email']);
+        $exp = new DateTime("now");
+        $exp->add(new DateInterval("PT3600S"));
         return $this->success([
             "data" => $user ,
-            "token" => $user->createToken($user->name)->plainTextToken
+            "token" => $user->createToken($user->name , ['*'] , $exp)->plainTextToken ,
+            "expire at" =>date("Y-m-d h:i:s" , $exp->getTimestamp()) ,
         ] , "User authenticated successfully");
     }
 }
--- api/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ api/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -16,6 +16,11 @@ return new class extends Migration
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
             $table->morphs('tokenable');
+            $table->foreign("tokenable_id")
+                                    ->on("users")
+                                    ->references("id")
+                                    ->onUpdate("cascade")
+                                    ->onDelete("cascade");
             $table->string('name');
             $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
