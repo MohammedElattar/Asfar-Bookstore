6579eb8cfc35e1849e0c29067e87645b29a89479
Mohamed Attar <mohammedattar0100020@gmail.com>
2022-12-04 21:13:49 +0200

    update api.php

--- api/routes/api.php
+++ api/routes/api.php
@@ -16,17 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::group([ 'prefix' => 'admin', 'namespace' => '\App\Http\Controllers\Api\Admin'], function () {
     // v1
     Route::group(['prefix' => 'v1', 'namespace' => 'V1' , 'middleware' => ['auth:sanctum']], function () {
-        Route::post("/books" , [books::class , 'index']);
+        // Route::post("/books" , [books::class , 'index']);
+        Route::apiResource("/categories", categoriesController::class)->withoutMiddleware("auth:sanctum");
     });
     Route::post("/login" , 'adminAuthController@login');
 });
-Route::get("/test" , function(){return "Hi";});
-
-Route::get("/login", function () {
-    return "This is login Page";
-})->name("login");
-
-
-// http://localhost:8000/api/dashboard
-
-Route::post("/dashboard" , function(){return "This is Dashboard";})->middleware("auth:sanctum");
