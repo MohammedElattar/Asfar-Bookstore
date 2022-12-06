2d201e16b7d9c59d629eb5445ea84f23e2b4668e
Mohamed Attar <mohammedattar0100020@gmail.com>
2022-12-06 09:50:05 +0200

    Update cors policy

--- api/config/cors.php
+++ api/config/cors.php
@@ -16,7 +16,7 @@ return [
 
     'paths' => ['*'],
 
-    'allowed_methods' => ['GET,POST,PUT,PATCH,DELETE,HEAD'],
+    'allowed_methods' => ['GET','POST','PUT','PATCH','DELETE','HEAD'],
 
     'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
 
