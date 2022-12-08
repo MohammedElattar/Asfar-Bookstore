2268a6b1765e1988d531bf106f273edabe77f379
Mohamed Attar <mohammedattar0100020@gmail.com>
2022-12-08 16:03:13 +0200

    Update .htaccess

--- api/app/Http/Controllers/Api/Admin/adminAuthController.php
+++ api/app/Http/Controllers/Api/Admin/adminAuthController.php
@@ -4,31 +4,39 @@ namespace App\Http\Controllers\Api\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\V1\authAdmin;
+use App\Http\Resources\Api\admin\v1\userResource;
 use App\Http\Traits\HttpResponse;
 use App\Models\User;
-use Illuminate\Http\Request;
-use App\Http\Requests\Admin\V1\authAdmin as authAdminReqeust;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class adminAuthController extends Controller
 {
     use HttpResponse;
+
     public function login(authAdmin $req)
     {
-        $credentials = $req->only('email', 'password');
-        if (!Auth::attempt($credentials, 1)) {
-            return $this->error('Authentication failed', 401 ,null);
+        $user = User::where('email', $req->email)->first(['id', 'email', 'password', 'user_role', 'password', 'name']);
+        if ($user->user_role == '1') {
+            if (Hash::check($req->password, $user->password)) {
+                Auth::loginUsingId($user->id, true);
+                unset($user->password);
+
+                return $this->success([
+                    new userResource($user),
+                ], 'User authenticated successfully');
+            } else {
+                return $this->error('Authentication failed', 401, includeData: false);
+            }
         }
-        $user = User::where('email', $credentials['email'])->first(['id', 'name', 'email']);
-        $exp = new \DateTime('now');
-        $exp->add(new \DateInterval('PT604800S'));
-        $token = $user->createToken('api token' , ['*'] , $exp)->plainTextToken;
-        return $this->success([
-            "user" => $user,
-        ], "User authenticated successfully");
+
+        return $this->error('Authentication failed', 401, null);
     }
-    public function logout(Request $req){
-        Auth::guard("web")->logout();
-        return $this->success(msg: "User logged out successfully");
+
+    public function logout()
+    {
+        Auth::guard('web')->logout();
+
+        return $this->success(msg: 'User logged out successfully');
     }
 }
