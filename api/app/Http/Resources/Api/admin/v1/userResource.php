f581a43fc4ddbaa79951561d1da3632d7a262f6b
Mohamed Attar <mohammedattar0100020@gmail.com>
2022-12-08 17:09:03 +0200

    add google oauth to config/services

---
+++ api/app/Http/Resources/Api/admin/v1/userResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\Api\admin\v1;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class userResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'email' => $this->email,
+            'name' => $this->name,
+            'admin' => $this->user_role ? true : false,
+        ];
+    }
+}
